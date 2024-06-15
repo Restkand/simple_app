aecb4bf597725e51b7409861f40aa59f471d8ef2
Restkand <restkand07@gmail.com>
2024-06-16 00:39:38 +0700

    Add Tailwind

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
+// src/index.js
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css'; // Pastikan file CSS diimpor di sini
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+<React.StrictMode>
+<App />
+</React.StrictMode>
+);
